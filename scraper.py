601f1bb1c86ecbc0fc0a2ce8c3756c7ad8ed400d
Montse <79296842+lapanquecita@users.noreply.github.com>
2023-03-12 19:15:22 -0600

    Add files via upload
    Changed PSAW to PMAW

--- scraper.py
+++ scraper.py
@@ -6,24 +6,24 @@ import argparse
 import csv
 from datetime import datetime
 
-from psaw import PushshiftAPI
+from pmaw import PushshiftAPI
 
-#used it for parsing default year
+# Used it for parsing default year
 x = datetime.now()
 
-#argparse object creation
-arg=argparse.ArgumentParser(description="Collets subreddits data")
+# argparse object creation
+arg = argparse.ArgumentParser(description="Collets subreddits data")
 arg.add_argument("-r", "--r",
-                type = str,
-                default = "Python",
-                help = " takes in name of the subreddit")
+                 type=str,
+                 default="Python",
+                 help=" takes in name of the subreddit")
 arg.add_argument("-yr", "--yr",
-                type = int,
-                default = x.year,
-                help = " takes in required Year")
-args=arg.parse_args()
+                 type=int,
+                 default=x.year,
+                 help=" takes in required Year")
+args = arg.parse_args()
 
-#constant variables
+# constant variables
 SUBREDDIT = args.r
 YEAR = args.yr
 
@@ -31,6 +31,7 @@ YEAR = args.yr
 # These are used to restrict by the year we are interested in.
 START_EPOCH = int(datetime(YEAR, 1, 1).timestamp())
 END_EPOCH = int(datetime(YEAR, 12, 31).timestamp())
+
 
 def main():
 
@@ -42,21 +43,19 @@ def main():
 
     gen = api.search_submissions(
         subreddit=SUBREDDIT,
-        after=START_EPOCH,
-        before=END_EPOCH,
+        since=START_EPOCH,
+        until=END_EPOCH,
         filter=["author", "title", "permalink"]
     )
 
     # We iterate over each object in the generator.
     for item in gen:
 
-        data = item[-1]
-
         # We extract the values and prevent crashes for non-existing ones.
-        timestamp = data["created_utc"]
-        author = data.get("author", "")
-        title = data.get("title", "")
-        permalink = data.get("permalink", "")
+        timestamp = item["created_utc"]
+        author = item.get("author", "")
+        title = item.get("title", "")
+        permalink = item.get("permalink", "")
 
         if permalink != "":
             permalink = "https://www.reddit.com" + permalink
@@ -68,7 +67,7 @@ def main():
 
     # We save the data list to a CSV file.
     with open(f"{SUBREDDIT}-{YEAR}.csv", "w", newline="", encoding="utf-8") as csv_file:
-        csv.writer(csv_file).writerows(data_list)
+        csv.writer(csv_file).writerows(sorted(data_list, reverse=True))
 
 
 if __name__ == "__main__":
